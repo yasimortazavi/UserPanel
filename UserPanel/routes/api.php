39a288a4503a33bed8bca50c013fdfbe352c97ce
yasimortazavi <73577026+yasimortazavi@users.noreply.github.com>
2024-12-20 18:30:35 +0330

    use apiResource in routs

--- UserPanel/routes/api.php
+++ UserPanel/routes/api.php
@@ -9,8 +9,11 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:api');
 
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
+// Route::get('/posts', [PostController::class, 'index']);
+// Route::post('/posts', [PostController::class, 'store']);
+// Route::get('/posts/{post}', [PostController::class, 'show']);
+// Route::put('/posts/{post}', [PostController::class, 'update']);
+// Route::delete('/posts/{post}', [PostController::class, 'delete']);
+
+//use api resource
+Route::apiResource('posts', PostController::class);
